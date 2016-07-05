f11f43687af118f12bd10e8c2d8ce388db3354b1
Junwei Yuan <jimyuan1995@outlook.com>
2016-07-05 16:56:33 +0100

    Undo added.

--- public/javascript/symbol.js
+++ public/javascript/symbol.js
@@ -6,11 +6,12 @@ function Symbol(text, x, y) {
 	this.default_y = y;
 }
 
-var symbols;
+var symbols,
+	padding = 15;
 function def_symbols() {
 	symbols = [];
-	symbols.push(new Symbol('A', 50, 50));
-	symbols.push(new Symbol('B', 80, 50));
+	symbols.push(new Symbol('A', padding, padding));
+	symbols.push(new Symbol('B', padding + 30, padding));
 }
 
 def_symbols();
